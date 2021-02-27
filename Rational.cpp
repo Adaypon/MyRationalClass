54f4e16f63da33ca00457b6732f313d7a64ddcad
Adaypon <75831434+Adaypon@users.noreply.github.com>
2021-02-27 17:34:54 +0300

    Init commit
    Init commit

---
+++ Rational.cpp
@@ -0,0 +1,210 @@
+#include "Rational.h"
+#include <iostream>
+
+int gcd(int a, int b) {
+	return b ? gcd(b, a % b) : a;
+}
+
+int lcm(int a, int b) {
+	return a / gcd(a, b) * b;
+}
+
+/**
+ * Конструкторы
+ */
+ 
+Rational::Rational() {
+    numerator = 1;
+    denumerator = 1;
+}
+
+Rational::Rational(int x, int y) {
+    numerator = x;
+    denumerator = y;
+
+    // Если y равен 0, значит выполнили где-то
+    // деление на 0
+    // Выставим nan в true
+    nan = (y == 0);
+}
+
+Rational::Rational(int x) {
+    numerator = x;
+    denumerator = 1;
+}
+
+Rational::Rational(const Rational& copy) {
+	this->nan = copy.nan;
+	this->numerator = copy.numerator;
+	this->denumerator = copy.denumerator;
+}
+
+Rational& Rational::operator=(const Rational& other) {
+	Rational copy(other);
+	std::swap(this->nan, copy.nan);
+	std::swap(this->numerator, copy.numerator);
+	std::swap(this->denumerator, copy.denumerator);
+	return *this;
+}
+
+
+// Сокращение
+Rational Rational::reduce() {
+	int del = gcd(numerator, denumerator);
+	if (del != 1) {
+		return Rational(
+			numerator / del,
+			denumerator / del
+		);
+	}
+	else {
+		return Rational(
+			numerator,
+			denumerator
+		);
+	}
+}
+
+// *-1
+Rational Rational::neg() {
+    return Rational(
+            -1 * numerator,
+            denumerator
+    );
+}
+
+// ^-1
+Rational Rational::inv() {
+    return Rational(
+        denumerator,
+        numerator
+    );
+}
+
+/**
+ * Арифмитические операции
+ */
+
+Rational Rational::sum(Rational r) {
+	int commonDenumerator = lcm(denumerator, r.denumerator); // общий знаменатель
+	return Rational(
+		numerator * (commonDenumerator / denumerator) + r.numerator * (commonDenumerator / r.denumerator),
+		commonDenumerator
+	).reduce(); // после сложения сокращаем число
+}
+
+Rational Rational::sub(Rational r) {
+	return sum(r.neg());
+}
+
+Rational Rational::mul(Rational r) {
+	return Rational(
+		numerator * r.numerator,
+		denumerator * r.denumerator
+	).reduce(); // после перемножения сокращаем число
+}
+
+Rational Rational::div(Rational r) {
+	return mul(r.inv());
+}
+
+/**
+ * Операции сравнения
+ */
+
+bool Rational::eq(Rational r) {
+    return
+        numerator * r.denumerator == r.numerator * denumerator;
+}
+
+bool Rational::neq(Rational r) {
+    return !eq(r);
+}
+
+bool Rational::lt(Rational r) {
+    return
+        numerator * r.denumerator < r.numerator * denumerator;
+}
+
+bool Rational::leq(Rational r) {
+    return
+        numerator * r.denumerator <= r.numerator * denumerator;
+}
+
+bool Rational::gt(Rational r) {
+    return
+        numerator * r.denumerator > r.numerator * denumerator;
+}
+
+bool Rational::geq(Rational r) {
+    return
+        numerator * r.denumerator >= r.numerator * denumerator;
+}
+
+/**
+ * служебные операции
+ */
+
+void Rational::print() {
+	/*
+	std::cout 
+        << numerator
+        << "/"
+        << denumerator
+        << std::endl;
+	*/
+	
+    if(nan)
+        std::cout << "<NAN>\n"
+		<< numerator
+        << "/"
+        << denumerator
+        << std::endl;
+    else
+        std::cout 
+            << numerator
+            << "/"
+            << denumerator
+            << std::endl;
+	
+}
+
+void Rational::scan() {
+    std::cin
+        >> numerator
+        >> denumerator;
+}
+
+int main() {
+	
+    Rational x; // 1/1
+    Rational y(10, 5); // 10/5
+    Rational z(7);
+	
+	x.print();
+	y.print();
+	z.print();
+	
+    std::cout << "x.eq(y): ";
+    std::cout << x.eq(y) << std::endl;
+    std::cout << "x.neq(y): ";
+    std::cout << x.neq(y) << std::endl;
+    std::cout << "z.neg(): ";
+    z.neg().print();
+	std::cout << "z.inv(): ";
+    z.inv().print();
+	std::cout << std::endl;
+    
+	Rational n(10, 0);
+	n.print();
+    n.mul(Rational(1)).print();
+    Rational(1).mul(n).print();
+	std::cout << std::endl;
+	
+	std::cout << "Enter rational number z: ";
+	z.scan();
+	z.print();
+	z.sum(y).reduce().print();
+	z.print();
+	
+}
---
+++ Rational.h
@@ -0,0 +1,60 @@
+#pragma once
+
+/**
+ * Дробь, x/y
+ */
+class Rational {
+public:
+    // not a number
+    // Указывает, что число не является числом
+    // по аналогии с float
+    // Устанавливается в true,
+    // если выполнили операцию
+    // с делением на ноль
+    bool nan = false;
+    int numerator;
+    int denumerator;
+
+    Rational();
+    Rational(int, int);
+    Rational(int);
+	
+	Rational(const Rational&);
+	
+	Rational& operator=(const Rational&);
+
+    // Сокращение
+    Rational reduce();
+
+    // *-1
+    Rational neg();
+    // ^-1
+    Rational inv();
+
+    Rational sum(Rational);
+    Rational sub(Rational);
+    Rational mul(Rational);
+    Rational div(Rational);
+
+    // ==
+    bool eq(Rational);
+    // !=
+    bool neq(Rational);
+    // less than
+    // <
+    bool lt(Rational);
+    // less or equal than
+    // <=
+    bool leq(Rational);
+    // greater than
+    // >
+    bool gt(Rational);
+    // greater or equal than
+    // >=
+    bool geq(Rational);
+
+    // Вывести на экран
+    void print();
+    // Считать с клавиатуры
+    void scan();
+};
